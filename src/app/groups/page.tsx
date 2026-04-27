a770dcdc1e6ca670009c68a7c3daca0c10bb35e9
logikmars <dima76673@gmail.com>
2026-04-27 13:38:40 +0300

    issue fix

--- src/app/groups/page.tsx
+++ src/app/groups/page.tsx
@@ -2,6 +2,9 @@ import './Group.scss';
 import { getReceipts } from '@/lib/receipts';
 import GroupsPage from '@/widgets/Groups/GroupsPage';
 
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 export default async function Page() {
   const receipts = await getReceipts();
 
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,13 +3,31 @@
 import { PropsWithChildren, useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { store } from '@/store';
-import { useAppSelector } from '@/store/hooks';
+import { useAppDispatch, useAppSelector } from '@/store/hooks';
 import i18n from '@/i18n/config';
+import { setLanguage, type Language } from '@/store/slices/languageSlice';
+
+const LANGUAGE_STORAGE_KEY = 'dzencode.language';
+const supportedLanguages: Language[] = ['ru', 'uk', 'en'];
+
+function isLanguage(value: string): value is Language {
+    return supportedLanguages.includes(value as Language);
+}
 
 const LanguageSync = ({ children }: PropsWithChildren) => {
+    const dispatch = useAppDispatch();
     const language = useAppSelector((state) => state.language.current);
 
     useEffect(() => {
+        const storedLanguage = window.localStorage.getItem(LANGUAGE_STORAGE_KEY);
+
+        if (storedLanguage && isLanguage(storedLanguage) && storedLanguage !== language) {
+            dispatch(setLanguage(storedLanguage));
+        }
+    }, [dispatch, language]);
+
+    useEffect(() => {
+        window.localStorage.setItem(LANGUAGE_STORAGE_KEY, language);
         void i18n.changeLanguage(language);
     }, [language]);
 
